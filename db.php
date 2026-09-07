ebc79fa4da0b1158d80d35d64aed5f7feae06d80
unknown <seowzhiqi5@gmail.com>
2026-09-07 16:02:47 +0800

    edit db.php

--- db.php
+++ db.php
@@ -6,10 +6,10 @@ date_default_timezone_set('Asia/Kuala_Lumpur');
 
 // MySQL Workbench 连接参数配置
 $db_host = 'localhost';      // 或 'localhost'
-$db_port = 3307;             // MySQL Workbench 默认端口
-$db_user = 'root';           // 你的 MySQL 用户名
-$db_pass = '12345';               // 你的 MySQL 密码
-$db_name = 'tnb_meter_db';   // 数据库名称
+$db_port = 3306;             // MySQL Workbench 默认端口
+$db_user = 'synergy1_yenping';           // 你的 MySQL 用户名
+$db_pass = 'R.zb0ZwEuGZ}*fW2';               // 你的 MySQL 密码
+$db_name = ' synergy1_zhiqi_TNB_Electricity_Meter_Monitoring_App';   // 数据库名称
 
 // 建立连接
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
